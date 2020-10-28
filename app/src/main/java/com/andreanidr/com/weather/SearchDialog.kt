6fa9c3b1f3640d179caec32b739d651206cccd8f
Douglas Andreani <andreani.dr@gmail.com>
2020-10-27 21:56:42 -0300

    protyping completed. need to add actual functionality to the app

---
+++ app/src/main/java/com/andreanidr/com/weather/SearchDialog.kt
@@ -0,0 +1,11 @@
+package com.andreanidr.com.weather
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+
+class SearchDialog : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_search_dialog)
+    }
+}
