6c191971cdf327bee898c2ec03ec610e931b4a7d
Douglas Andreani <andreani.dr@gmail.com>
2020-11-11 21:30:04 -0300

    improved imports and fix typos

--- app/src/main/java/com/andreanidr/com/weather/ui/MainActivity.kt
+++ app/src/main/java/com/andreanidr/com/weather/ui/MainActivity.kt
@@ -2,20 +2,18 @@ package com.andreanidr.com.weather.ui
 
 
 import android.os.Bundle
-import android.util.Log
 import android.widget.TextView
 import androidx.appcompat.app.AppCompatActivity
 import androidx.lifecycle.Observer
 import androidx.lifecycle.ViewModelProvider
-import androidx.lifecycle.lifecycleScope
-import androidx.lifecycle.observe
 import com.andreanidr.com.weather.Internals.Injectors
 import com.andreanidr.com.weather.R
-
-import com.andreanidr.com.weather.model.CurrentWeather
 import com.andreanidr.com.weather.viewmodel.CurrentWeatherViewModel
 import com.andreanidr.com.weather.viewmodel.CurrentWeatherViewModelFactory
-import kotlinx.coroutines.*
+import kotlinx.coroutines.CoroutineScope
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.Job
+import kotlinx.coroutines.launch
 import kotlin.coroutines.CoroutineContext
 
 
@@ -40,8 +38,6 @@ class MainActivity : AppCompatActivity(), CoroutineScope {
 
     override fun onResume() {
         super.onResume()
-        Log.d("OnResume", "onResume: called")
-
         currentWeatherViewModelFactory = Injectors.provideCurrentWeatherViewModelFactory(applicationContext, "")
         viewModel = ViewModelProvider(this, currentWeatherViewModelFactory).get(CurrentWeatherViewModel::class.java)
 
@@ -52,9 +48,8 @@ class MainActivity : AppCompatActivity(), CoroutineScope {
     private fun bindUI() = launch {
         val cityName = findViewById<TextView>(R.id.city)
         val temp = findViewById<TextView>(R.id.Temp)
-        val d = viewModel.currentWeather
-        val currentweather = viewModel.currentWeather
-        currentweather.observe(this@MainActivity, Observer {
+        val currentWeather = viewModel.currentWeather
+        currentWeather.observe(this@MainActivity, Observer {
             if (it == null) return@Observer
             cityName.text = it.name
             temp.text = it.main.temp.toString()
